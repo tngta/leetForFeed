75a827040399bfedca2c8cab93ebbcdff27df2b7
Brian Ta <tab1@nku.edu>
2026-07-31 16:22:35 -0500

    Length of Longest Substr

---
+++ slidingWindow/longestSubstr.py
@@ -0,0 +1,16 @@
+s = "abcabcbb"
+
+# => 3
+
+visited = set()
+l= 0
+res = 0
+
+for r in range(len(s)):
+    while s[r] in visited:
+        visited.remove(s[l])
+        l+=1
+
+    visited.add(s[r])
+    res = max(res, r-l+1)
+print(res)
